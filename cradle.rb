23332fb011e9abaddfda7a46e0a8af7ddfc9b740
Robert Hanacek <robert@idea-loop.com>
2011-12-27 23:45:19 -0600

    Simplified cradle, added initial "translator" :D

--- cradle.rb
+++ cradle.rb
@@ -1,62 +1,70 @@
-module Cradle
-  TAB = "\t"
-  attr_accessor :look
-
-  def get_char
-    @look = STDIN.getc
-  end
-
-  def error(msg)
-    puts "Error: #{msg}."
-  end
-
-  def abort(with_msg)
-    error with_msg
-    Process.exit
-  end
-
-  def expected(s)
-    abort "expected '#{s}'"
-  end
-
-  def match(c)
-    if @look == c[0]
-      get_char
-    else 
-      expected c
-    end
-  end
-
-  def Cradle.is_alpha(c)
-    return ('a'..'z').include? c.downcase
-  end
-
-  def Cradle.is_digit(c)
-    return ('0'..'9').include? c
-  end
-
-  def get_num()
-    return 7
-  end
+def expected(expected, actual)
+  "expected #{expected}, got '#{actual}'"
 end
 
+def match(x, y)
+  x == y
+end
+
+def is_alpha(c)
+  ('a'..'z').include? c.downcase
+end
+
+def is_digit(c)
+  ('0'..'9').include? c
+end
+
+def get_num(c)
+  return c if is_digit c
+  raise ArgumentError, expected('integer', c)
+end
+
+def emit(s) 
+  puts "\t#{s}"
+end
+
+def expression c
+  emit "MOVE #{get_num c},D0"
+end
+
+#main 
+
+puts "enter symbol, 'q' to quit"
+print '> '
+while (c = gets.chomp()) != 'q' do
+  expression c
+  print '> '
+end
+
+=begin
 require 'test/unit'
 
 class CradleTest < Test::Unit::TestCase
+  def test_get_num
+    assert_block do
+      ('0'..'9').all? { |c| (get_num c) == c }
+    end
+
+    assert_raise ArgumentError do
+      get_num 'a'
+    end
+  end
+
   def test_is_alpha
     assert_block do
-      ('a'..'z').all? { |c| Cradle.is_alpha c }
-      ('A'..'Z').all? { |c| Cradle.is_alpha c }
-      ('1'..'9').none? { |c| Cradle.is_alpha c }
-      ('!@#$%^&*()_+-=').none? { |c| Cradle.is_alpha c }
+      ('a'..'z').all? { |c| is_alpha c }
+      ('A'..'Z').all? { |c| is_alpha c }
+      ('1'..'9').none? { |c| is_alpha c }
+      ('!@#$%^&*()_+-=').none? { |c| is_alpha c }
     end
   end
 
   def test_is_digit
     assert_block do
-      ('0'..'9').all? { |c| Cradle.is_digit c }
-      ('a'..'z').none? { |c| Cradle.is_digit c }
-      ('A'..'Z').none? { |c| Cradle.is_digit c }
+      ('0'..'9').all? { |c| is_digit c }
+      ('a'..'z').none? { |c| is_digit c }
+      ('A'..'Z').none? { |c| is_digit c }
     end
   end
 end
+=end
